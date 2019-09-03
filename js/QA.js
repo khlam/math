580c399f148dae47c4c545c39877f35eeb857ac5
khlam <4841220+khlam@users.noreply.github.com>
2019-09-03 10:21:47 -0700

    display question

--- js/QA.js
+++ js/QA.js
@@ -1,16 +1,16 @@
 function display(data){
     switch(data.t) {
         case 'addition':
-            console.log("not impl")
+            console.log('not impl')
             break;
         case 'subtraction':
-            console.log("not impl")
+            console.log('not impl')
             break;
         case 'multiplication':
-            console.log("not impl")
+            console.log('not impl')
             break;
         case 'division':
-            return `<div class='col-12'><h2>${data.a} / ${data.b} = </h2></div>`
+            return `<div id='currentQuestion' class='col-12' class='col-12' style='text-align: center;'><h1>${data.a} / ${data.b} = </h1></div>`
         default:
             return
     }
