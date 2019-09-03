b08d98b280eddd462678bb21d7bd4501075afe7f
khlam <4841220+khlam@users.noreply.github.com>
2019-09-02 21:14:16 -0700

    fix

--- js/divison.js
+++ js/divison.js
@@ -4,28 +4,44 @@ function generateDivision(param) { // wait why do this just multiply the divisor
         let divisionDividend_UPPER = param.init.divisionDividend_UPPER
         let divisionDivisor_LOWER = param.init.divisionDivisor_LOWER
         let divisionDivisor_UPPER = param.init.divisionDivisor_UPPER
-
-        let divisor = rand(divisionDivisor_LOWER, divisionDivisor_UPPER)
-        getPossibleDividends(divisor, divisionDividend_UPPER, divisionDivisor_LOWER).then(allDividends => {
-            return dividend = allDividends[rand(0, allDividends.length - 1)]
-        }).then(dividend => {
-            if (dividend != undefined){
+        getPossibleDivisor(divisionDivisor_LOWER, divisionDivisor_UPPER, divisionDividend_UPPER).then(divisor => {
+            getPossibleDividends(divisor, divisionDividend_UPPER, divisionDivisor_LOWER).then(allDividends => {
+                return allDividends.filter((x) => { return x >= divisionDividend_LOWER})
+            }).then(prunedDividends => {
+                console.log(prunedDividends)
+                return prunedDividends[rand(0, prunedDividends.length - 1)]
+            }).then(dividend => {
                 console.log(dividend)
-                return resolve(
-                    {
-                        a: dividend,
-                        b: divisor,
-                        c: dividend / divisor,
-                    })
-            }
+                console.log(divisor)
+                if (dividend != undefined){
+                    return resolve(
+                        {
+                            a: dividend,
+                            b: divisor,
+                            c: dividend / divisor,
+                        })
+                    }
+                })
         })
+    })
+}
+
+function getPossibleDivisor(divisionDivisor_LOWER, divisionDivisor_UPPER, divisionDividend_UPPER) {
+    return new Promise((resolve) => {
+        let divisorCheck = true;
+        while(divisorCheck) {
+            let divisor = rand(divisionDivisor_LOWER, divisionDivisor_UPPER)
+            if (divisor <= divisionDividend_UPPER) {
+                return resolve(divisor)
+            }
+        }
     })
 }
 
 function getPossibleDividends(divisor, divisionDividend_UPPER, divisionDivisor_LOWER) {
     return new Promise((resolve) => {
         let dividends = []
-        for (i = 1; i < divisionDividend_UPPER; i++) {
+        for (i = 1; i <= divisionDividend_UPPER; i++) {
             val = divisor * i
             if (val <= divisionDividend_UPPER && val >= divisionDivisor_LOWER) {
                 dividends.push(divisor * i)
@@ -34,7 +50,6 @@ function getPossibleDividends(divisor, divisionDividend_UPPER, divisionDivisor_L
             }
         }
     })
-
 }
 
 // Randomly prune 1
